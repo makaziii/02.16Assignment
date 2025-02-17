08679757b25ccc5cf718866bd0951f4eeff59625
Janat Kazibwe <janat.kazibwe@mnsu.edu>
2025-02-16 18:16:26 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static int CountWords(string sentence)
+    {
+        if (sentence == "")
+            return 0;
+
+        string[] words = sentence.Split(' ');
+        return words.Length;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(CountWords("Just an example here move along"));
+        Console.WriteLine(CountWords("This is a test"));
+        Console.WriteLine(CountWords("What an easy task, right"));
+    }
+}
